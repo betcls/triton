bb4e93d70ab60ae71639f7a11e08a166b6f0e541
zephyr <Bernard_Claeys@skynet.be>
2017-08-29 14:00:23 +0200

    test01

--- src/main/java/boodschap/server/BoodschapServlet.java
+++ src/main/java/boodschap/server/BoodschapServlet.java
@@ -37,10 +37,10 @@ public class BoodschapServlet extends HttpServlet {
 				 * lokaal werken.
 				 */
 
-			String host = System.getenv("mysql_SERVICE_HOST");
-			String port = System.getenv("mysql_SERVICE_PORT");
-			String mysqlUser = System.getenv("MYSQL_USER");
-			String mysqlPassword = System.getenv("MYSQL_PASSWORD");
+			String host = System.getenv("MYSQL_SERVICE_HOST");
+			String port = System.getenv("MYSQL_SERVICE_PORT");
+			String mysqlUser = "root" ; //System.getenv("MYSQL_USER");
+			String mysqlPassword = "LJq3O4wjrMCbA6bd" ; //System.getenv("MYSQL_PASSWORD");
 
 			if (host != null || port != null) {
 				sqlContext = new MySqlContext(host + ":" + port, "Boodschap", mysqlUser, mysqlPassword);
